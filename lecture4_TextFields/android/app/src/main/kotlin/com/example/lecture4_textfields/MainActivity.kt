3bd6c3de095aefcff85ee4255cc7333efbdcd701
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2026-07-03 15:31:53 +0500

    lecture 4 textfields complete

---
+++ lecture4_TextFields/android/app/src/main/kotlin/com/example/lecture4_textfields/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lecture4_textfields
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lecture4_TextFields/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:lecture4_textfields/loginUI.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Learning',
+      theme: ThemeData(
+
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: LoginUI(),
+    );
+  }
+}
