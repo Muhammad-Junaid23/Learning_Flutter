bdf22c83aceb4e6411457f1518b016239de17d33
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2026-07-03 22:43:16 +0500

    lecture 4 task signup UI complete

--- lecture4_TextFields/lib/main.dart
+++ lecture4_TextFields/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lecture4_textfields/loginUI.dart';
+import 'package:lecture4_textfields/registerUI.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,8 @@ class MyApp extends StatelessWidget {
 
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: LoginUI(),
+      // home: LoginUI(),
+      home: RegisterUI(),
     );
   }
 }
