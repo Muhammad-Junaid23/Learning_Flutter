b83974f2448142c859c9fc81a9ed83e0fa549cca
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2026-07-04 10:41:33 +0500

    lecture 5 pageview complete

---
+++ lecture5_pageview/android/app/src/main/kotlin/com/example/lecture5_pageview/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lecture5_pageview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lecture5_pageview/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:lecture5_pageview/pageview.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Learning',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Pageview(),
+    );
+  }
+}
