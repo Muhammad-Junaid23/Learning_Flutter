9fe2916d97211ab9acb02d55c33974ccb750d4d0
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2026-07-11 12:35:52 +0500

    bottom bar practice

---
+++ lecture11_bottombar/lib/bottombar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:lecture11_bottombar/Home.dart';
+import 'package:lecture11_bottombar/profile.dart';
+import 'package:lecture11_bottombar/settings.dart';
+
+class BottomBarScreen extends StatefulWidget {
+  const BottomBarScreen({super.key});
+
+  @override
+  State<BottomBarScreen> createState() => _BottomBarScreenState();
+}
+
+class _BottomBarScreenState extends State<BottomBarScreen> {
+  List<Widget> screenList = [
+    Home(),
+    Profile(),
+    Settings(),
+  ];
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+        title: Text("Bottom Bar"),
+      ),
+      body: screenList.elementAt(selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.black,
+        backgroundColor: Colors.blue,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        iconSize: 30,
+        onTap: (val){
+          setState(() {
+            selectedIndex = val;
+          });
+        },
+        currentIndex: selectedIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings),label: "Settings"),
+          BottomNavigationBarItem(icon: Icon(Icons.person),label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
---
+++ lecture11_bottombar/lib/settings.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 20,),
+            settingTile(Colors.red, Icons.email, "Email"),
+            settingTile(
+              Colors.green,
+              Icons.person,
+              "USERNAME",
+            ),
+
+            settingTile(
+              Colors.blueGrey,
+              Icons.lock_outline,
+              "PASSWORD",
+            ),
+
+            settingTile(
+              Colors.deepPurple,
+              Icons.call,
+              "CONTACT",
+            ),
+
+            settingTile(
+              Colors.blue,
+              Icons.bookmark_border,
+              "SAVED",
+            ),
+
+            settingTile(
+              Colors.cyan,
+              Icons.logout,
+              "LOGOUT",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+Widget settingTile(
+    Color color,
+    IconData icon,
+    String title,
+    ) {
+  return Container(
+    margin: EdgeInsets.symmetric(
+      vertical: 10,
+      horizontal: 15,
+    ),
+    padding: EdgeInsets.symmetric(
+      horizontal: 20,
+      vertical: 18,
+    ),
+    decoration: BoxDecoration(
+      color: color,
+      borderRadius: BorderRadius.circular(18),
+    ),
+    child: Row(
+      children: [
+
+        Icon(
+          icon,
+          color: Colors.white,
+        ),
+
+        SizedBox(width: 15),
+
+        Expanded(
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+        ),
+
+        Icon(
+          Icons.arrow_forward,
+          color: Colors.white,
+        ),
+
+      ],
+    ),
+  );
+}
