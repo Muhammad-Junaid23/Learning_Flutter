6941a7d87d4d9a571b4b5a7e7a2f8a19e8654154
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2026-07-30 23:01:31 +0500

    countries model and service added

---
+++ lecture_backend_crud/lib/models/country_model.dart
@@ -0,0 +1,26 @@
+
+
+class CountryModel{
+  final String? docId;
+  final String? countryName;
+  final int? createdAt;
+
+  CountryModel({
+    this.docId,
+    this.countryName,
+    this.createdAt,
+  });
+
+  factory CountryModel.fromJson(Map<String, dynamic> json) => CountryModel(
+    docId: json["docId"],
+    countryName: json["countryName"],
+    createdAt: json["createdAt"],
+  );
+
+  Map<String, dynamic> toJson(String countryID) =>{
+       "docId":docId,
+        "countryName": countryName,
+        "createdAt":createdAt,
+      };
+
+}
