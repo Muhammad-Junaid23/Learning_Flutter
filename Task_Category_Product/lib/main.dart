4626a5c3f22abe7b18534c3d94e603d6af416bf2
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2026-08-04 12:43:55 +0500

    initial project setup

---
+++ Task_Category_Product/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:task_category_product/views/product/all_products.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Product_Category',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: AllProducts(),
+    );
+  }
+}
+
